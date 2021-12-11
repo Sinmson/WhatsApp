b1a8094aff1502ac0b39395fc781cce478715b66
Tim Pütz <puetz_tim@web.de>
2021-12-11 12:59:27 +0000

    Init first draft without components of WhatsApp frontend

--- apps/whats-app/src/app/app.component.ts
+++ apps/whats-app/src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@test/api-interfaces';
+import { Message } from '@whatsapp/api-interfaces';
 
 @Component({
-  selector: 'test-root',
+  selector: 'wa-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   hello$ = this.http.get<Message>('/api/hello');
+
+  dark = true;
+  chatIsOpen = false;
+  chatSymbolsAreOpen = false;
+  chatSymboOpen = 0;
+
   constructor(private http: HttpClient) {}
+
+  public CloseChat() {
+    this.chatIsOpen = false;
+  }
+
+  public OpenChat() {
+    this.chatIsOpen = true;
+  }
+
+  public OpenChatSymbols() {
+    this.chatSymbolsAreOpen = true;
+    this.chatSymboOpen = 1;
+  }
+  
+  public CloseChatSymbols() {
+    this.chatSymbolsAreOpen = false;
+    this.chatSymboOpen = 0;
+  }
 }
