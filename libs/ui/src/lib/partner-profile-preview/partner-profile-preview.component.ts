5a274879115e04c7e90f8e719c54882dc8bb865a
Tim Pütz <puetz_tim@web.de>
2021-12-14 16:50:07 +0000

    Add ContextMenu to partner-profile component

--- libs/ui/src/lib/partner-profile-preview/partner-profile-preview.component.ts
+++ libs/ui/src/lib/partner-profile-preview/partner-profile-preview.component.ts
@@ -1,31 +1,48 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ContextMenuService } from "../context-menu/context-menu.service";
 
 @Component({
   selector: 'wa-partner-profile-preview',
   templateUrl: './partner-profile-preview.component.html',
   styleUrls: ['./partner-profile-preview.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush 
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PartnerProfilePreviewComponent  {
+export class PartnerProfilePreviewComponent {
 
   /**
    *  Define if the open chat fits to this chat preview
    */
-   @Input()  
-   openChat: any = null;
- 
-   @Output() 
-   openChatChange = new EventEmitter<any>();
+  @Input()
+  openChat: any = null;
 
-   public get OpenChat() { return this.openChat; }
-   public set OpenChat(val: any) { this.openChat =  val; this.openChatChange.emit(val); }
+  @Output()
+  openChatChange = new EventEmitter<any>();
 
-   
-  // constructor() { }
+  public get OpenChat() { return this.openChat; }
+  public set OpenChat(val: any) { this.openChat = val; this.openChatChange.emit(val); }
+
+  protected _ContextMenuEntries: string[] = ["Kontaktinfo", "Nachrichten auswählen", "Chat schließen", "Benachr. stummschalten", "Nachrichten leeren", "Chat löschen"];
+  public get ContextMenuEntries() { return this._ContextMenuEntries; }
+  public set ContextMenuEntries(v: string[]) { this._ContextMenuEntries = v; }
+
+
+  constructor(private contextMenuService: ContextMenuService) { }
+
+  public async OpenContextMenu(e: Event) {
+    console.log("UserProfilePreviewComponent | selectedIndex");
+    e.stopPropagation(); // To not trigger click of the chat preview div 
+    const selectedIndex = await this.contextMenuService.Open(e.target as HTMLElement, this.ContextMenuEntries, "bottom left");
+    console.log("UserProfilePreviewComponent | selectedIndex", selectedIndex);
+
+    if(selectedIndex === 2) {  // Chat schließen
+      this.CloseChat();
+    }
+  }
+
 
   public CloseChat() {
     this.OpenChat = null;
   }
-  
+
 
 }
