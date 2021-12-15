53b0f69b532e52319a4ecf8a460c557dbade3489
Tim Pütz <puetz_tim@web.de>
2021-12-15 13:13:58 +0000

    Aadd configurable properties to partner-profile component

--- libs/ui/src/lib/partner-profile-preview/partner-profile-preview.component.stories.ts
+++ libs/ui/src/lib/partner-profile-preview/partner-profile-preview.component.stories.ts
@@ -7,16 +7,66 @@ export default {
   decorators: [
     moduleMetadata({
       imports: [],
-    })
+    })    
   ],
+  argTypes: {
+    lastOnline: {
+      defaultValue: null,
+      description: "Last online date of user or null",
+      control: {
+        type: "date"
+      },
+    }
+  }
 } as Meta<PartnerProfilePreviewComponent>;
 
 const Template: Story<PartnerProfilePreviewComponent> = (args: PartnerProfilePreviewComponent) => ({
   component: PartnerProfilePreviewComponent,
-  props: args,
+  props: {
+    ...args,
+    lastOnline: args.lastOnline ? new Date(args.lastOnline) : null
+  },
 });
 
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Default = Template.bind({});
+Default.args = {
+}
+
+export const WithProfile = Template.bind({});
+WithProfile.args = {
+  ...Default.args,
+  lastOnline: null,
+  openChat: { id: 0 },
+  name: "Maria Musterfrau",
+  isOnline: false,
+  profilePicSrc: "https://i.pravatar.cc/40?u=MariaMusterfrau"
+}
+
+export const WithProfileOnline = Template.bind({});
+WithProfileOnline.args = {
+  ...WithProfile.args,
+  lastOnline: new Date(),
+  isOnline: true
+}
+
+export const WithProfileOfflineLong = Template.bind({});
+WithProfileOfflineLong.args = {
+  ...WithProfile.args,
+  lastOnline: new Date(2000, 10, 23, 13, 37, 420),
+  isOnline: false
+}
+
+export const WithProfileOfflineYesterday = Template.bind({});
+WithProfileOfflineYesterday.args = {
+  ...WithProfile.args,
+  lastOnline: new Date(new Date().setDate(new Date().getDate()-1)),
+  isOnline: false
+}
+
+export const WithProfileOfflineToday = Template.bind({});
+WithProfileOfflineToday.args = {
+  ...WithProfile.args,
+  lastOnline: new Date(new Date().setHours(new Date().getHours()-1)),
+  isOnline: false
 }
--- libs/ui/src/lib/partner-profile-preview/partner-profile-preview.component.ts
+++ libs/ui/src/lib/partner-profile-preview/partner-profile-preview.component.ts
@@ -15,8 +15,41 @@ export class PartnerProfilePreviewComponent {
   @Input()
   openChat: any = null;
 
+  @Input()
+  name = "<Name>";
+
+  @Input()
+  lastOnline : Date | null = null;
+
+  @Input()
+  profilePicSrc = "";
+
+  @Input()
+  isOnline = false;
+
   @Output()
   openChatChange = new EventEmitter<any>();
+
+  public get StatusString() {
+    if(this.isOnline) {
+      return "Online";
+    }
+
+    if(!this.lastOnline) {
+      return "";
+    } else {
+      const diffInMs = Date.now() - this.lastOnline.getTime();
+      if(diffInMs < 0) {
+        return "";
+      } else if(( diffInMs) >  (1000 * 60 * 60 * 24 * 2) ) {
+        const dateStr = this.lastOnline.toLocaleDateString();
+        return `zuletzt online am ${dateStr}`;
+      } 
+      const todayOrYesterday = this.lastOnline.getDay() === new Date().getDay() ? "heute" : "gestern";
+      const hoursStr = this.lastOnline.toLocaleTimeString( [ navigator.language, "en-US"], { hour: '2-digit', minute:'2-digit'} );
+      return `zuletzt online ${todayOrYesterday} um ${hoursStr}`;
+    }
+  }
 
   public get OpenChat() { return this.openChat; }
   public set OpenChat(val: any) { this.openChat = val; this.openChatChange.emit(val); }
@@ -26,7 +59,8 @@ export class PartnerProfilePreviewComponent {
   public set ContextMenuEntries(v: string[]) { this._ContextMenuEntries = v; }
 
 
-  constructor(private contextMenuService: ContextMenuService) { }
+  constructor(private contextMenuService: ContextMenuService) {    
+  }
 
   public async OpenContextMenu(e: Event) {
     console.log("UserProfilePreviewComponent | selectedIndex");
